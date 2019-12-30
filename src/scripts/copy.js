d7c999183f762cab308ca1bd43e0852238ba02e8
julienC <julien.cadeau@adyoulike.com>
2019-12-30 16:54:51 +0100

    new way to load copy

---
+++ src/scripts/copy.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+    //copy to clipboard event on items
+  	function copyToClipboard(element) {
+		 var $temp = $("<input>");
+		 $("body").append($temp);
+		 $temp.val($(element).html()).select();
+		 document.execCommand("copy");
+		 $temp.remove();
+	};
+  	$(".copy").click(function(){
+  		var copied = $(event.target).closest("li").find(".campaign_id");
+  		copyToClipboard(copied);
+  		$("#errormessage").slideDown().text("Copied");
+  		setTimeout(function(){
+			$('#errormessage').slideUp();
+		}, 2000);
+  	});
+});
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -138,22 +138,7 @@ $(document).ready(function() {
 	  	});
 	},2000);
 
-  	//copy to clipboard event on items
-  	function copyToClipboard(element) {
-		 var $temp = $("<input>");
-		 $("body").append($temp);
-		 $temp.val($(element).html()).select();
-		 document.execCommand("copy");
-		 $temp.remove();
-	};
-  	$(".copy").click(function(){
-  		var copied = $(event.target).closest("li").find(".campaign_id");
-  		copyToClipboard(copied);
-  		$("#errormessage").slideDown().text("Copied");
-  		setTimeout(function(){
-			$('#errormessage').slideUp();
-		}, 2000);
-  	});
+  	
 
   	$("#like_filter").click(function(){
   		if (($("#like_filter").hasClass("selected"))){
